d98880d040c16238d07cccb226ae83fe31dc6590
Rodrigo Franco <rodrigo.pfranco49@edu.unifor.br>
2023-12-02 13:59:54 -0300

    Inicio_Celeiro

---
+++ scripts/objeto_celeiro.gd
@@ -0,0 +1,11 @@
+extends StaticBody2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	print($Sprite2D.transform)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
