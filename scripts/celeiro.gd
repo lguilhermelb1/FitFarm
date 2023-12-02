d2c53bb8679e6f6db39a46df9a257b51566c238e
Rodrigo Franco <rodrigo.pfranco49@edu.unifor.br>
2023-12-02 20:04:06 -0300

    abertura_portas

--- scripts/celeiro.gd
+++ scripts/celeiro.gd
@@ -1,6 +1,8 @@
 extends StaticBody2D
 class_name barn
 
+var permitido = false
+
 func lotado():
 	var cont = 0
 		
@@ -9,3 +11,30 @@ func lotado():
 			cont += 1
 			
 	return cont == 4
+
+func _on_area_porta_body_entered(body):
+	if body.name == "player_world":
+		play_open_door()
+
+func _on_area_porta_body_exited(body):
+	permitido = false
+	play_open_door()
+
+
+func play_open_door():
+	var sprite = $Sprite2D.texture.get_path().get_file().replace(".png", "")
+	print(sprite)
+	if sprite == "celeiro_aberto":
+		$AnimationPlayer.play_backwards("door")
+		await($AnimationPlayer.animation_finished)
+		
+	elif sprite == "celeiro_fechado":
+		$AnimationPlayer.play("door")
+		await($AnimationPlayer.animation_finished)
+	
+	
+	
+	
+	
+
+
