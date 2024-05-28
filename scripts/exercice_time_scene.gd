773a6dff1dc1ed33c8f7643da03c0952d0eaac2b
IngridMoreira <ingridmc.0813@gmail.com>
2024-05-28 01:15:17 -0300

    nova interface da tela de exercicios

--- scripts/exercice_time_scene.gd
+++ scripts/exercice_time_scene.gd
@@ -1,65 +1,109 @@
-extends Node2D
+extends Control
 
-@onready var transition_animation := $transition/animation
+@onready var grid_container = %GridContainer
+@onready var error_message = %Error_message
+@onready var error_time_message = %Error_Time_Message
+@onready var pin = %PIN
+@onready var tempo = %Tempo
+@onready var scroll_container = %ScrollContainer
+var keyboard_height = 0
 
-# Called when the node enters the scene tree for the first time.
+func _process(delta):
+	if(DisplayServer.has_feature(DisplayServer.FEATURE_VIRTUAL_KEYBOARD)):
+		var new_keyboard_height = DisplayServer.virtual_keyboard_get_height()
+		if(keyboard_height == 0 and new_keyboard_height != 0):
+			keyboard_height = new_keyboard_height
+			scrollcontainer_fit_keyboard(true)
+		elif(keyboard_height != 0  and new_keyboard_height == 0):
+			keyboard_height = new_keyboard_height
+			scrollcontainer_fit_keyboard(false)
+			#
 func _ready():
-	get_window().size = Vector2(640, 320)
-	await(transition_animation.is_playing())
-	
-	$animation.play("opening")
-	$Control/PIN.editable=false
-	$Control/Tempo.editable=false
-	$Control/Button.disabled=true
-	
-	$Control/Error_message.visible=false
-	$Control/Error_message.text=""
-	$Control/Error_Time_Message.visible=false
-	$Control/Error_Time_Message.text=""
-	
-	await($animation.is_playing() == false)
-	
-	if name == 'Scene_SetTime':
-		await(get_tree().create_timer(3).timeout)
-	else:
-		await(get_tree().create_timer(5).timeout)
-	
-	$animation.play("criacao_pin")
-	await($animation.is_playing())
-	$Control/PIN.editable=true
-	$Control/Tempo.editable=true
-	
-	var item_list = get_node("res://prefab/label_set_time/Control/ItemList")
-	
-	# Verifique se a referência não é nula
-	if item_list != null:
-		$Control/Button.disabled=true
-	else:
-		$Control/Button.disabled=false
+	process_mode = Node.PROCESS_MODE_ALWAYS
+	get_tree().paused = false
+	error_message.visible=false
+	error_message.text=""
+	error_time_message.visible=false
+	error_time_message.text=""
+	const exercise_list = [
+		{"label": "Apoio de Frente", "image": "flexão.png"},
+		{"label": "Rosca com Peso", "image": "rosca.png"},
+		{"label": "Arremessar bola (acompanhado ou só)", "image": "Arremessar bola.png"},
+		{"label": "Sentar e Levantar", "image": "sentar e levantar.png"},
+		{"label": "Corrida Lateral", "image": "corrida.png"},
+		{"label": "Saltos (Pular corda ou afins)", "image": "saltos.png"},
+		{"label": "Outros", "image": "Outros.png"}
+	]
+	for exercise in exercise_list:	
+		var texture = load("res://assets/Exercicies/" + exercise["image"])
+		var texture_rect = TextureRect.new()
+		texture_rect.texture = texture
+		grid_container.add_child(texture_rect)
 
+		
+		var label = Label.new()
+		label.add_theme_font_size_override("font_size", 30)
+		label.text = exercise["label"]
+		
+	
+		grid_container.add_theme_constant_override("h_separation", 50)
+		grid_container.add_theme_constant_override("v_separation", 30)
+		grid_container.add_child(label)
 
 func _on_button_pressed():
 	
-	if Global.pin != $Control/PIN.text:
-		$Control/Error_message.text = "PIN INVÁLIDO, INSIRA NOVAMENTE"
-		$Control/Error_message.visible=true
+	if Global.pin != pin.text:
+		error_message.text = "PIN INVÁLIDO, INSIRA NOVAMENTE"
+		error_message.visible=true
 		await(get_tree().create_timer(3).timeout)
-		$Control/Error_message.visible=false
+		error_message.visible=false
 	
-	if $Control/Tempo.text == "":
-		$Control/Error_Time_Message.text = "TEMPO LIMITE INVÁLIDO, INSIRA NOVAMENTE"
-		$Control/Error_Time_Message.visible=true
+	elif tempo.value == 0:
+		error_time_message.text = "TEMPO LIMITE INVÁLIDO, INSIRA NOVAMENTE"
+		error_time_message.visible=true
 		await(get_tree().create_timer(3).timeout)
-		$Control/Error_Time_Message.visible=false
+		error_time_message.visible=false
 		
-	elif int($Control/Tempo.text) < 20 or int($Control/Tempo.text) > 120 and $Control/Tempo.editable == true:
-		$Control/Error_Time_Message.text = "O TEMPO LIMITE DEVE SER ENTRE 20 e 120 minutos"
-		$Control/Error_Time_Message.visible=true
+	elif int(tempo.value) < 20 or int(tempo.value) > 120 and tempo.editable == true:
+		error_time_message.text = "O TEMPO LIMITE DEVE SER ENTRE 20 e 120 minutos"
+		error_time_message.visible=true
 		await(get_tree().create_timer(3).timeout)
-		$Control/Error_Time_Message.visible=false
+		error_time_message.visible=false
 		
-	if Global.pin == $Control/PIN.text and $Control/Tempo.text != "" \
-	and int($Control/Tempo.text) >= 20 and int($Control/Tempo.text) <= 120:
-		Global.atualizar_tempo_transicao(int($Control/Tempo.text)*60)
+	else: 
+		Global.atualizar_tempo_transicao(int(tempo.value)*60)
 		Global.setTransition($transition)
-		$transition.change_scene("res://scenes/mundo_01.tscn")
+		change_scene("res://scenes/mundo_01.tscn")
+
+
+
+func change_scene(path: String):
+	get_tree().paused = true
+	_update_values()
+	assert(get_tree().change_scene_to_file(path) == OK)
+
+
+func _update_values():
+	if get_tree().current_scene.name == "Mundo01":
+		for x in Global.lista:
+			if 'position' in x.keys():
+				for v in get_tree().get_nodes_in_group("vegetable"):
+					if v.global_position == x['position']:				
+						v.get_node("timer").paused=true
+						x['current_time'] = v.get_node("timer").time_left	
+						x['status'] = v.get_status()
+						
+					
+
+func scrollcontainer_fit_keyboard(is_keyboard_visible:bool, ):
+	if is_keyboard_visible:
+		scroll_container.size.y -= 300
+		await get_tree().process_frame
+		var scroller = scroll_container.get_v_scroll_bar()
+		scroller.value = scroller.max_value
+	else:
+		scroll_container.size.y += 300
+	
+
+
+
