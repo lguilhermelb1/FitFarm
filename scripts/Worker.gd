5853bb0a2668b43ab331b22d592b52e2bcf95387
Rodrigo Franco <rodrigo.pfranco49@edu.unifor.br>
2024-05-30 17:46:00 -0300

    ajuste_fontes_ajuste_problema_compras

--- scripts/Worker.gd
+++ scripts/Worker.gd
@@ -9,8 +9,8 @@ func _on_area_2d_body_entered(body):
 	if body.is_in_group("player"):
 		_dialog_screen =  load("res://prefab/dialog_node.tscn").instantiate()
 		
-		_dialog_screen.transform.origin[0] = transform.origin[0] - 56
-		_dialog_screen.transform.origin[1] = transform.origin[1] - 70
+		_dialog_screen.transform.origin[0] = transform.origin[0] - 90
+		_dialog_screen.transform.origin[1] = transform.origin[1] - 120
 		get_tree().get_root().add_child(_dialog_screen)
 		
 func _on_area_2d_body_exited(body):
