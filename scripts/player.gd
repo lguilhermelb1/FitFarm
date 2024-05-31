db0296d11c99f83f45c285c941fd9af85efc0d14
IngridMoreira <ingridmc.0813@gmail.com>
2024-05-31 18:02:35 -0300

    mudando forma de movimentação no bug invaders

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,10 +3,8 @@ class_name Player extends CharacterBody2D
 signal spray_shot(shoot_scene, location)
 signal died
 
-@export var speed = 300
-
+@export var speed = 50
 @export var shot_speed := 0.25
-
 @onready var muzzle = $Muzzle
 
 var shoot_scene = preload("res://scenes/shoot.tscn")
@@ -17,28 +15,6 @@ var hp = 3
 func _ready():
 	$cristal_score.transform['x'][0] = 2
 	$cristal_score.transform['y'][1] = 2
-	print($cristal_score.transform)
-
-
-func _process(delta):
-	if Input.is_action_pressed("shoot"):
-		if !shoot_cd:
-			shoot_cd = true
-			shoot()
-			await get_tree().create_timer(shot_speed).timeout
-			shoot_cd = false
-
-
-func _physics_process(_delta):
-	var direction = Vector2(Input.get_axis("ui_left", "ui_right"), 
-	Input.get_axis("ui_up", "ui_down"))
-	velocity = direction * speed
-	move_and_slide()
-	
-	#var pos = Vector2(get_viewport_rect().size[0] -20, get_viewport_rect().size[1]+40)
-	#print(pos)
-	
-	global_position = global_position.clamp(Vector2.ZERO, get_viewport_rect().size)
 
 
 func shoot():
@@ -55,3 +31,20 @@ func set_cristal_score_label(value:int):
 	$cristal_score/anim.play("fade_in")
 	$audio.play()
 	await ($cristal_score/anim.animation_finished)
+
+func _input(event):
+	if event is InputEventScreenDrag: # get final position on release of mouse button
+		var direction = event.relative
+		velocity = Vector2(direction.x, 0) * speed
+		move_and_slide()
+		global_position = global_position.clamp(Vector2(20, 20), get_viewport_rect().size - Vector2(20, 20) )
+	elif event is InputEventScreenTouch:
+		shoot()
+	if !shoot_cd:
+			shoot_cd = true
+			shoot()
+			await get_tree().create_timer(shot_speed).timeout
+			shoot_cd = false
+
+	
+	
