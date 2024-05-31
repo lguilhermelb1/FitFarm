5b5c91392e4e55d208acb176ca46036de7e01255
IngridMoreira <ingridmc.0813@gmail.com>
2024-05-31 18:06:49 -0300

    limpando codigo

--- scripts/hud.gd
+++ scripts/hud.gd
@@ -7,10 +7,3 @@ extends Control
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	process_mode = Node.PROCESS_MODE_ALWAYS
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-
