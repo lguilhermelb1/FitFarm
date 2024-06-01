9ffa983835db6c5012f0312e205d185678a2ce1a
IngridMoreira <ingridmc.0813@gmail.com>
2024-05-31 21:16:54 -0300

    ajuste HUD

--- scripts/hud.gd
+++ scripts/hud.gd
@@ -1,9 +1,17 @@
 extends Control
 
+signal exit_button_pressed
+
+@onready var exit_button = $ExitButton
+
 @onready var score = $Score:
 	set(value):
 		score.text = "SCORE: " + str(value)
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	process_mode = Node.PROCESS_MODE_ALWAYS
+
+
+
+func _on_exit_button_pressed():
+	exit_button_pressed.emit()
